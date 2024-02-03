7fbc07e6860556d9123e982743d1f07a9ec7b936
iyiola ladejo <iyioladejo@gmail.com>
2024-02-03 14:48:43 +0100

    Show a spinner while fetching the genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import useGenres, { Genre } from '../hooks/useGenres';
-import {ListItem, SimpleGrid, Text, List, HStack, Image} from '@chakra-ui/react'
+import {ListItem, SimpleGrid, Text, List, HStack, Image, Spinner} from '@chakra-ui/react'
 import GameCardContainer from './GameCardContainer';
 import GameCardSkeleton from './GameCardSkeleton';
 import useData from '../hooks/useData';
@@ -8,11 +8,13 @@ import getCroppedImageUrl from '../services/image-url';
 
 const GenreList = () => {
     const {data, error, isLoading, setError, setData} = useGenres();
-    const skeletons = [1,2,3,4,5,6]
+    //if (isLoading) return <Spinner></Spinner>
 
   return (
     <>
         <List>
+            {isLoading && <Spinner/>}
+            {error && <Text>{null}</Text>}
             {data.map((genre) => 
             <ListItem key={genre.id} paddingY='5px'>
                 <HStack>
