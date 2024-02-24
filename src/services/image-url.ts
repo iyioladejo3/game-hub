13101b8d37ab786dd71e8a3dd89b9b971d418d3e
iyiola ladejo <iyioladejo@gmail.com>
2024-02-24 14:25:43 +0100

    sort the games

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 
 const getCroppedImageUrl = (url:string) => {
     //url is the originalWorld
+    if (!url) return '';
     const target = 'media/'
     const index = url.indexOf(target) + target.length; //Position to insert the word
     const stringToInsert= 'crop/600/400/';
