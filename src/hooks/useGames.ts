d5efa4d63fd93e2c92f9a6530830d5e7843dce1d
iyiola ladejo <iyioladejo@gmail.com>
2024-02-25 07:57:24 +0100

    Add emojis

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -11,9 +11,10 @@ export interface Game{
     id:number;
     name:string;
     background_image:string;
-    rating:number;
     parent_platforms:{platform: Platform};
     metacritic:number;
+    rating_top: number;
+    rating:number;
 };
 
 
